ab910ca24b55304ac99778a8abea34c41c94f31e
lapskaya-ks <k.lapskay@gmail.com>
2021-06-08 19:20:08 +0300

    Внесла правки в функцию

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 function getRandom (min, max) {
 
-  if (min >= max) {
+  if (min < 0 || max < 0 || min >= max) {
     return 'Появится какая-нибудь ошибка';
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandom (100, 90);
+getRandom (5, 90);
 
-
-function getArbitrary(min, max, maxSymbol = 0) {
-  if (min >= max) {
+function getArbitrary (min, max, maxSymbol = 0) {
+  if (min < 0 || max < 0 || min >= max) {
     return ('Задан неверный диапазон! Нужно указать другое число');
   }
 
